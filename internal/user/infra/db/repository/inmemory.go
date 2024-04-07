424360c3bbc2a63af047912cfc7b15336764c28f
André Santos <andre@dexdev.com.br>
2024-04-07 08:16:48 -0300

    refactor: from Db to InMemoryContextRepo name

---
+++ internal/user/infra/db/repository/inmemory.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"errors"
+	userEntity "github.com/dexfs/go-twitter-clone/internal/user"
+	"github.com/dexfs/go-twitter-clone/pkg/database"
+)
+
+type InMemoryUserRepo struct {
+	db *database.InMemoryDB[userEntity.User]
+}
+
+func NewUserInMemoryRepo(db *database.InMemoryDB[userEntity.User]) *InMemoryUserRepo {
+	return &InMemoryUserRepo{
+		db: db,
+	}
+}
+
+func (r *InMemoryUserRepo) ByUsername(username string) (*userEntity.User, error) {
+	for _, currentUser := range r.db.GetAll() {
+		if currentUser.Username == username {
+			return currentUser, nil
+		}
+	}
+
+	return nil, errors.New("currentUser not found")
+}
+
+func (r *InMemoryUserRepo) Insert(item *userEntity.User) {
+	r.db.Insert(item)
+}
+
+func (r *InMemoryUserRepo) GetAll() []*userEntity.User {
+	return r.db.GetAll()
+}
+
+func (r *InMemoryUserRepo) Remove(item *userEntity.User) {
+	r.db.Remove(item)
+}
