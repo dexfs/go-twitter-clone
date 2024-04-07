f64d1ead72d6891df3b464608f3a051142c4b96f
André Santos <andre@dexdev.com.br>
2024-04-07 16:00:06 -0300

    feat: test mocks

---
+++ tests/mocks/mocks.go
@@ -0,0 +1,51 @@
+package mocks
+
+import (
+	postEntity "github.com/dexfs/go-twitter-clone/internal/posts"
+	postDomainInterfaces "github.com/dexfs/go-twitter-clone/internal/posts/domain/interfaces"
+	postInfraImpl "github.com/dexfs/go-twitter-clone/internal/posts/infra/db/repository"
+	userEntity "github.com/dexfs/go-twitter-clone/internal/user"
+	"github.com/dexfs/go-twitter-clone/internal/user/domain/interfaces"
+	"github.com/dexfs/go-twitter-clone/internal/user/infra/db/repository"
+	"github.com/dexfs/go-twitter-clone/pkg/database"
+	"strconv"
+)
+
+// mocks
+func MakeDb[T any]() *database.InMemoryDB[T] {
+	return &database.InMemoryDB[T]{}
+}
+func MakeInMemoryUserRepo(db *database.InMemoryDB[userEntity.User]) interfaces.UserRepository {
+	repo := repository.NewInMemoryUserRepo(db)
+	return repo
+}
+func MakeInMemoryPostRepo(db *database.InMemoryDB[postEntity.Post]) postDomainInterfaces.PostRepository {
+	repo := postInfraImpl.NewInMemoryPostRepo(db)
+	return repo
+}
+func UserSeed(db *database.InMemoryDB[userEntity.User], amount int) []*userEntity.User {
+	if amount <= 0 {
+		amount = 1
+	}
+	users := make([]*userEntity.User, amount)
+	for i := 0; i < len(users); i++ {
+		username := "user" + strconv.Itoa(i)
+		newUser := userEntity.NewUser(username)
+		db.Insert(newUser)
+		users[i] = newUser
+	}
+	return users
+}
+func PostSeed(db *database.InMemoryDB[postEntity.Post], user *userEntity.User) []*postEntity.Post {
+	posts := make([]*postEntity.Post, 2)
+	for i := 0; i < len(posts); i++ {
+		newPostInput := postEntity.NewPostInput{
+			User:    user,
+			Content: "post_" + strconv.Itoa(i),
+		}
+		newPost, _ := postEntity.NewPost(newPostInput)
+		db.Insert(newPost)
+		posts[i] = newPost
+	}
+	return posts
+}
