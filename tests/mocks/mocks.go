ab850302128592b9ca21b67f4452d7102da85246
André Santos <andre@dexdev.com.br>
2024-04-08 21:13:55 -0300

    chore(test): helpers

--- tests/mocks/mocks.go
+++ tests/mocks/mocks.go
@@ -36,8 +36,8 @@ func UserSeed(db *database.InMemoryDB[userEntity.User], amount int) []*userEntit
 	}
 	return users
 }
-func PostSeed(db *database.InMemoryDB[postEntity.Post], user *userEntity.User) []*postEntity.Post {
-	posts := make([]*postEntity.Post, 2)
+func PostSeed(db *database.InMemoryDB[postEntity.Post], user *userEntity.User, amount int) []*postEntity.Post {
+	posts := make([]*postEntity.Post, amount)
 	for i := 0; i < len(posts); i++ {
 		newPostInput := postEntity.NewPostInput{
 			User:    user,
@@ -48,4 +48,25 @@ func PostSeed(db *database.InMemoryDB[postEntity.Post], user *userEntity.User) [
 		posts[i] = newPost
 	}
 	return posts
+}
+
+type TestMocks struct {
+	MockUserDB    *database.InMemoryDB[userEntity.User]
+	MockUserSeed  []*userEntity.User
+	MockPostDB    *database.InMemoryDB[postEntity.Post]
+	MockPostsSeed []*postEntity.Post
+}
+
+func GetTestMocks() TestMocks {
+	mockUserDB := MakeDb[userEntity.User]()
+	mockPostDB := MakeDb[postEntity.Post]()
+	mockUserSeed := UserSeed(mockUserDB, 1)
+	mockPostsSeed := PostSeed(mockPostDB, mockUserSeed[0], 2)
+
+	return TestMocks{
+		MockUserDB:    mockUserDB,
+		MockUserSeed:  mockUserSeed,
+		MockPostDB:    mockPostDB,
+		MockPostsSeed: mockPostsSeed,
+	}
 }
