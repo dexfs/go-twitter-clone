91b8fcdcdfb07665c382c06b4abc7bb2bc7b096f
André Santos <andre@dexdev.com.br>
2024-04-13 13:50:01 -0300

    feat: helpers for JSON encoding

---
+++ internal/application/handlers/helpers.go
@@ -0,0 +1,25 @@
+package handlers
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+)
+
+func JSON(w http.ResponseWriter, statusCode int, data interface{}) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	if data != nil {
+		if err := json.NewEncoder(w).Encode(data); err != nil {
+			log.Fatal(err)
+		}
+	}
+}
+
+func JSONError(w http.ResponseWriter, statusCode int, err error) {
+	JSON(w, statusCode, struct {
+		Erro string `json:"error"`
+	}{
+		Erro: err.Error(),
+	})
+}
