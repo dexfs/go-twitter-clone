fbae2fcba9c9c128b5e48084900f915b2c7a65ff
André Santos <andre@dexdev.com.br>
2024-05-11 15:53:24 -0300

    refactor: rename router files and remove dependencies initialization

---
+++ adapter/input/routes/gin_router.go
@@ -0,0 +1,60 @@
+package routes
+
+import (
+	"github.com/dexfs/go-twitter-clone/adapter/input/adapter_http"
+	"github.com/dexfs/go-twitter-clone/internal/core/port/output"
+	"github.com/dexfs/go-twitter-clone/internal/core/usecase"
+	"github.com/fvbock/endless"
+	"github.com/gin-gonic/gin"
+)
+
+type AppServer struct {
+	Router *gin.Engine
+	addr   string
+}
+
+func NewRouter(addr string) *AppServer {
+	//gin.SetMode(gin.DebugMode)
+	//gin.ForceConsoleColor()
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(200, "pong")
+	})
+
+	return &AppServer{
+		Router: r,
+		addr:   addr,
+	}
+}
+
+func (s *AppServer) Run(userRepo output.UserPort, postRepo output.PostPort) error {
+	err := s.Router.SetTrustedProxies(nil)
+	if err != nil {
+		return err
+	}
+
+	s.InitUserResources(userRepo, postRepo)
+	s.InitPostResources(userRepo, postRepo)
+	return endless.ListenAndServe(s.addr, s.Router)
+}
+func (s *AppServer) InitUserResources(userRepo output.UserPort, postRepo output.PostPort) {
+	getUserInfoService, _ := usecase.NewGetUserInfoUseCase(userRepo)
+	getUserFeedUseCase, _ := usecase.NewGetUserFeedUseCase(userRepo, postRepo)
+
+	usersController := adapter_http.NewUsersController(getUserInfoService, getUserFeedUseCase)
+
+	s.Router.GET("/users/:username/info", usersController.GetInfo)
+	s.Router.GET("/users/:username/feed", usersController.GetFeed)
+}
+
+func (s *AppServer) InitPostResources(userRepo output.UserPort, postRepo output.PostPort) {
+	createPostUseCase, _ := usecase.NewCreatePostUseCase(postRepo, userRepo)
+	createRepostUseCase, _ := usecase.NewCreateRepostUseCase(postRepo, userRepo)
+	createQuoteUseCase, _ := usecase.NewCreateQuoteUseCase(postRepo, userRepo)
+
+	postsController := adapter_http.NewPostsController(createPostUseCase, createRepostUseCase, createQuoteUseCase)
+
+	s.Router.POST("/posts", postsController.CreatePost)
+	s.Router.POST("/posts/repost", postsController.CreateRepost)
+	s.Router.POST("/posts/quote", postsController.CreateQuote)
+}
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,8 +2,45 @@ package main
 
 import (
 	"github.com/dexfs/go-twitter-clone/adapter/input/routes"
+	"github.com/dexfs/go-twitter-clone/adapter/output/repository/inmemory"
+	inmemory_schema "github.com/dexfs/go-twitter-clone/adapter/output/repository/inmemory/schema"
+	"github.com/dexfs/go-twitter-clone/internal/core/port/output"
+	"github.com/dexfs/go-twitter-clone/pkg/database"
 	"log"
+	"time"
 )
+
+var (
+	db       *database.InMemoryDB
+	userRepo output.UserPort
+	postRepo output.PostPort
+)
+
+func init() {
+	db = database.NewInMemoryDB()
+	if db == nil {
+		log.Fatal("database is nil")
+	}
+
+	initialUsers := make([]*inmemory_schema.UserSchema, 2)
+	initialUsers = append(initialUsers, &inmemory_schema.UserSchema{
+		ID:        "4cfe67a9-defc-42b9-8410-cb5086bec2f5",
+		Username:  "alucard",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+	initialUsers = append(initialUsers, &inmemory_schema.UserSchema{
+		ID:        "b8903f77-5d16-4176-890f-f597594ff952",
+		Username:  "alexander",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	db.RegisterSchema(inmemory.USER_SCHEMA_NAME, initialUsers)
+	db.RegisterSchema(inmemory.POST_SCHEMA_NAME, []*inmemory_schema.PostSchema{})
+	userRepo = inmemory.NewInMemoryUserRepository(db)
+	postRepo = inmemory.NewInMemoryPostRepository(db)
+}
 
 type APIServer struct {
 	addr string
@@ -17,7 +54,7 @@ func NewAPIServer(addr string) *APIServer {
 
 func (s *APIServer) Run() error {
 	router := routes.NewRouter(s.addr)
-	return router.Run()
+	return router.Run(userRepo, postRepo)
 }
 
 func main() {
