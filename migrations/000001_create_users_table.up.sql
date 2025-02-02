ec18764a718f0a2711d541fb051d28dfa166955d
André Santos <andre@dexdev.com.br>
2025-02-02 16:58:20 -0300

    feat: migrations

--- migrations/000001_create_users_table.up.sql
+++ migrations/000001_create_users_table.up.sql
@@ -1,6 +1,6 @@
 CREATE TABLE users
 (
-    id         VARCHAR(36) PRIMARY KEY, -- Sem default, deve ser fornecido manualmente, uuid V7: ex: 0194b7b8-1110-79c0-8d41-cff79b880911
+    user_id         VARCHAR(36) PRIMARY KEY, -- Sem default, deve ser fornecido manualmente, ulid : ex: 01JJYY0V9AMD9656HT4BSV0ZEK
     username   varchar(50) NOT NULL UNIQUE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT NOW(),
     updated_at TIMESTAMPTZ NOT NULL DEFAULT NOW()
