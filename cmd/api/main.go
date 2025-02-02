10676cc4b92a7d1b6738a92763f327350ba39dd3
André Santos <andre@dexdev.com.br>
2025-02-02 16:57:02 -0300

    feat: use postgres repository

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,8 +3,6 @@ package main
 import (
 	"context"
 	"github.com/dexfs/go-twitter-clone/adapter/input/routes"
-	"github.com/dexfs/go-twitter-clone/adapter/output/repository/inmemory"
-	inmemory_schema "github.com/dexfs/go-twitter-clone/adapter/output/repository/inmemory/schema"
 	"github.com/dexfs/go-twitter-clone/adapter/output/repository/postgres"
 	"github.com/dexfs/go-twitter-clone/internal/core/port/output"
 	"github.com/dexfs/go-twitter-clone/pkg/database"
@@ -47,25 +45,9 @@ func init() {
 		log.Fatal("database is nil")
 	}
 
-	//initialUsers := make([]*inmemory_schema.UserSchema, 0)
-	//initialUsers = append(initialUsers, &inmemory_schema.UserSchema{
-	//	ID:        "4cfe67a9-defc-42b9-8410-cb5086bec2f5",
-	//	Username:  "alucard",
-	//	CreatedAt: time.Now(),
-	//	UpdatedAt: time.Now(),
-	//})
-	//initialUsers = append(initialUsers, &inmemory_schema.UserSchema{
-	//	ID:        "b8903f77-5d16-4176-890f-f597594ff952",
-	//	Username:  "alexander",
-	//	CreatedAt: time.Now(),
-	//	UpdatedAt: time.Now(),
-	//})
-
-	//db.RegisterSchema(inmemory.USER_SCHEMA_NAME, initialUsers)
-	db.RegisterSchema(inmemory.POST_SCHEMA_NAME, []*inmemory_schema.PostSchema{})
-	//userRepo = inmemory.NewInMemoryUserRepository(db)
 	userRepo = postgres.NewPostgresUserRepository(pgDB)
-	postRepo = inmemory.NewInMemoryPostRepository(db)
+	postRepo = postgres.NewPostgresPostRepository(pgDB)
+
 }
 
 type APIServer struct {
